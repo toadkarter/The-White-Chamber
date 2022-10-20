05108428b1d9e5d84f7048550972e000888eb121
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-10-20 12:06:42 +0100

    added player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController _playerBody = null;
+    private Camera _playerCamera = null;
+    private float _xRotation = 0f;
+    private bool _canMove = true;
+
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float mouseSensitivity = 100f;
+    
+    private void Start()
+    {
+        _playerBody = GetComponent<CharacterController>();
+        _playerCamera = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+        if (!_canMove) return;
+        Move();
+        Look();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        var direction = transform.right * x + transform.forward * z;
+        _playerBody.Move(direction * speed * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        
+        _playerCamera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        _playerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    public void SetCanMove(bool canMove)
+    {
+        if (canMove)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _canMove = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _canMove = false;
+        }
+    }
+}
