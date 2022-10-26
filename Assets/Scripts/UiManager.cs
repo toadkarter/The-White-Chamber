256f560062660ce617e508751282b9b6afc6f842
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-10-26 13:41:59 +0100

    added movement manager

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    private Image _pointer = null;
+    private readonly Color _onColor = Color.red;
+    private readonly Color _offColor = Color.white;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pointer = GetComponentInChildren<Image>();
+    }
+
+    public void togglePointer()
+    {
+        _pointer.color = _pointer.color == _offColor ? _onColor : _offColor;
+    }
+}
